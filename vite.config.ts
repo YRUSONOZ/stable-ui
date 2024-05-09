abe76c0358dd70337a961b6dcaecf79e5aaacc95
YRUSONOZ <74988686+YRUSONOZ@users.noreply.github.com>
2024-05-09 00:08:15 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig({
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
-    base: '/', // Adjust this base path
+    base: '/stable-ui/', // Update this base path
 });
